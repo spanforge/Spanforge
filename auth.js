7730a35a260c34d76bf129ea772cfb4a1bcf0307
viswanathanstartup <mystartup.vis@gmail.com>
2026-03-28 21:28:18 +0530

    login fixed

--- auth.js
+++ auth.js
@@ -1,7 +1,6 @@
 import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import Google from 'next-auth/providers/google'
-import GitHub from 'next-auth/providers/github'
 import { prisma } from '@/lib/prisma'
 
 const providers = []
@@ -11,15 +10,6 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    })
-  )
-}
-
-if (process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET) {
-  providers.push(
-    GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     })
   )
 }
